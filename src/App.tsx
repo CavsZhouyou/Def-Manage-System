3e50348980c4b5d1fd8123b2048e98caaab92f47
Cavs Zhouyou <zhouyou.world@outlook.com>
2020-03-07 16:18:04 +0800

    feat: 添加 ant-design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.scss';
+import { Button } from 'antd';
+// import './App.scss';
 
 export default function App() {
   return (
@@ -39,7 +40,11 @@ export default function App() {
 }
 
 function Home() {
-  return <h2 className="title">Home</h2>;
+  return (
+    // <h2 className="title">
+    <Button type="primary">Primary</Button>
+    // </h2>
+  );
 }
 
 function About() {
