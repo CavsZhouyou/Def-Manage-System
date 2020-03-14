106529f56e1fbd674bd515c3b2fac28f35131a55
Cavs Zhouyou <zhouyou.world@outlook.com>
2020-03-14 12:04:29 +0800

    feat: 应用详情添加头部导航栏

--- src/pages/AppDetail/index.tsx
+++ src/pages/AppDetail/index.tsx
@@ -4,34 +4,49 @@
  * @TodoList: 无
  * @Date: 2020-03-13 19:09:13
  * @Last Modified by: zhouyou@werun
- * @Last Modified time: 2020-03-13 20:24:36
+ * @Last Modified time: 2020-03-14 12:03:29
  */
 
 import React, { memo } from 'react';
-import { Tabs } from 'antd';
+import { Link } from 'react-router-dom';
+import { Tabs, Breadcrumb } from 'antd';
 import styles from './index.module.scss';
 import routes from './config';
 
 const { TabPane } = Tabs;
 
+const NavBar = (): JSX.Element => {
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item>
+        <Link to="/appList">应用列表</Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Item>ihome-fe/design-service</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
+
 export default memo(function AppDetail() {
   return (
     <div className={styles.appDetail}>
-      <Tabs defaultActiveKey="1" animated={true}>
-        {routes.map(route => (
-          <TabPane
-            tab={
-              <span>
-                {route.icon()}
-                {route.name}
-              </span>
-            }
-            key={route.key}
-          >
-            {route.component()}
-          </TabPane>
-        ))}
-      </Tabs>
+      <NavBar />
+      <div className={styles.contentWrapper}>
+        <Tabs defaultActiveKey="1" animated={true}>
+          {routes.map(route => (
+            <TabPane
+              tab={
+                <span>
+                  {route.icon()}
+                  {route.name}
+                </span>
+              }
+              key={route.key}
+            >
+              {route.component()}
+            </TabPane>
+          ))}
+        </Tabs>
+      </div>
     </div>
   );
 });
